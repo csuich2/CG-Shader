5d8e5b3905d95c41df67ce352457f66ea3869140
Chris Suich <csuich2@gmail.com>
2012-11-07 23:27:21 -0500

    Added support for multiple lights Lights should be specified in the 'lights.txt', one per line in the format: (x, y, z), (Ka, Kd, Ks)

--- Assignment3/Assignment3.cpp
+++ Assignment3/Assignment3.cpp
@@ -1,3 +1,10 @@
+// Code here is adopted from the NVIDIA examples:
+//	09_vertex_lighting and
+//	11_two_lights_with_structs
+// as well as:
+//	http://codepad.org/rqZIKFfQ
+//	my Assignment2, which was based on other sources
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <gl/glut.h>
@@ -5,6 +12,11 @@
 #include "common.h"
 #include <Cg/cg.h>
 #include <Cg/cgGl.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
 using namespace Raytracer;
 
 #define APP_NAME "Assignment 3"
@@ -17,10 +29,19 @@ int showReflectionColor = 0;
 int showRefractedColor = 0;
 int lightOverhead = 1;
 
-GLfloat light_ambient[] = {1.0, 1.0, 1.0};
-GLfloat light_specular[] = {1.0, 1.0, 1.0};
-GLfloat light_diffuse[] = {1.0, 1.0, 1.0};
-GLfloat light_position[] = {0.0, 5.0, 0.0, 1};
+GLfloat default_light_ambient[] = {1.0, 1.0, 1.0};
+GLfloat default_light_specular[] = {1.0, 1.0, 1.0};
+GLfloat default_light_diffuse[] = {1.0, 1.0, 1.0};
+GLfloat default_light_position[] = {0.0, 5.0, 0.0, 1};
+
+struct LightInfo {
+	GLfloat position[3];
+	GLfloat La;
+	GLfloat Ld;
+	GLfloat Ls;
+};
+
+vector<LightInfo> light_info;
 
 GLMmodel *model;
 
@@ -32,7 +53,8 @@ static CGprofile   myCgVertexProfile,
 				   myCgFragmentProfile;
 static CGprogram   myCgVertexProgram,
 				   myCgFragmentProgram;
-static CGparameter myCgFragmentParam_eyePosition;
+static CGparameter myCgFragmentParam_eyePosition,
+				   myCgFragmentParam_lightCount;
 
 static const char *myVertexProgramFileName = "vertex.cg",
 				  *myVertexProgramName = "main",
@@ -44,6 +66,7 @@ void glutKeyboard(unsigned char key, int x, int y);
 void glutSpecial(int key, int xx, int yy);
 void glutResize(int width, int height);
 void getObjModel();
+void getLights();
 
 static void checkForCgError(const char *situation)
 {
@@ -57,12 +80,13 @@ static void checkForCgError(const char *situation)
 	  const char *compError = cgGetLastListing(myCgContext);
       printf("%s\n", compError);
     }
-    exit(1);
+    //exit(1);
   }
 }
 
 void main() {
 	getObjModel();
+	getLights();
 
 	glutInitWindowSize(winW, winH);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
@@ -118,14 +142,32 @@ void main() {
 	checkForCgError("could not get " #name " parameter");
 
 	GET_PARAM(eyePosition);
+	GET_PARAM(lightCount);
 
 	cgGLSetParameter3f(myCgFragmentParam_eyePosition, 0,0,-2);
+	cgGLSetParameter1d(myCgFragmentParam_lightCount, light_info.size()==0 ? 1 : light_info.size());
 	cgUpdateProgramParameters(myCgFragmentProgram);
 
-	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	if (light_info.size() == 0) {
+		glLightfv(GL_LIGHT0, GL_SPECULAR, default_light_specular);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, default_light_diffuse);
+		glLightfv(GL_LIGHT0, GL_AMBIENT, default_light_ambient);
+		glLightfv(GL_LIGHT0, GL_POSITION, default_light_position);
+	} else {
+		int i=0;
+		while (!light_info.empty()) {
+			LightInfo info = light_info.back();
+			light_info.pop_back();
+			GLfloat ambient[3]  = {info.La, info.La, info.La};
+			GLfloat diffuse[3]  = {info.Ld, info.Ld, info.Ld};
+			GLfloat specular[3] = {info.Ls, info.Ls, info.Ls};
+			glLightfv(GL_LIGHT0+i, GL_POSITION, info.position);
+			glLightfv(GL_LIGHT0+i, GL_SPECULAR, ambient);
+			glLightfv(GL_LIGHT0+i, GL_DIFFUSE, diffuse);
+			glLightfv(GL_LIGHT0+i, GL_AMBIENT, specular);
+			i++;
+		}
+	}
 	glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
 
 	//glEnable(GL_LIGHTING);
@@ -169,10 +211,10 @@ void glutKeyboard(unsigned char key, int x, int y) {
 	} else if (key == 'l' || key == 'L') {
 		lightOverhead = !lightOverhead;
 		if (lightOverhead)
-			light_position[2] = 0.0f;
+			default_light_position[2] = 0.0f;
 		else
-			light_position[2] = -5.0f;
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+			default_light_position[2] = -5.0f;
+		glLightfv(GL_LIGHT0, GL_POSITION, default_light_position);
 		printf("Light overhead? %d\n", lightOverhead);
 	} else if (key == 'u' || key == 'U') {
 		printf("Unitizing models\n");
@@ -252,8 +294,26 @@ void glutDisplay() {
 }
 
 void getObjModel() {
-	model = glmReadOBJ("input.obj");
+	model = glmReadOBJ("objects/gourd.obj");
 	glmFacetNormals(model);
 	glmVertexNormals(model, 89); // 89 because 90 creates weird shadows on flat surfaces
 	//glmUnitize(model);
+}
+
+void getLights() {
+	ifstream file;
+	file.open("lights.txt");
+	if (!file)
+		return;
+	string line;
+	while (getline(file, line)) {
+		if (line == "" || line[0] == '#')
+			continue;
+		
+		LightInfo info;
+		sscanf(line.c_str(), "(%f, %f, %f), (%f, %f, %f)",
+			&info.position[0], &info.position[1], &info.position[2],
+			&info.La, &info.Ld, &info.Ls);
+		light_info.push_back(info);
+	}
 }
